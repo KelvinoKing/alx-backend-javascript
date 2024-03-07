7a507e8a8c712418416df62c6b9bbe63c298ac15
KelvinoKing <kelvinoking@outlook.com>
2024-03-07 23:16:41 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,5 +5,5 @@ export default function handleProfileSignup() {
     .then((photo) => createUser()
       .then((user) => {
         console.log(photo.body, user.firstName, user.lastName);
-      }));
+      })).catch(() => console.log('Signup system offline'));
 }
