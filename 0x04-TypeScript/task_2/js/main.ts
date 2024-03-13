9c3ba2150af783c84a6cb00750a353efc7b2b8c7
KelvinoKing <kelvinoking@outlook.com>
2024-03-13 22:51:38 +0300

    Creating functions specific to employees

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,58 @@
+export interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+export interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
+}
+
+export class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to teacher tasks';
+    }
+}
+
+export function createEmployee(salary: number | string): Teacher | Director {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}   
+
+export function isDirector(employee: Director | Teacher) {
+    return employee instanceof Director;
+}
+
+export function executeWork(employee: Director | Teacher) {
+    if (isDirector(employee)) {
+        return (employee as Director).workDirectorTasks();
+    }
+
+    return (employee as Teacher).workTeacherTasks();
+}
