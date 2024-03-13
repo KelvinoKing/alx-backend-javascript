57fb0474cfb26b02c1f0744117ed2c1d3c541781
KelvinoKing <kelvinoking@outlook.com>
2024-03-13 21:47:12 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,30 @@ export interface printTeacherFunction {
 export function printTeacher(firstName: string, lastName: string): string {
 	return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+export interface StudentConstructor {
+	new(firstName: string, lastName: string): StudentClass;
+}
+
+export interface StudentClassInterface {
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+export class StudentClass {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return 'Currently working';
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
