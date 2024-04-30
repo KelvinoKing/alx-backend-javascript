64ad799420d3d13ab410412db44cff0e677526e5
KelvinoKing <kelvinoking@outlook.com>
2024-04-30 19:03:40 +0300

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// create a function named displayMessage that prints in STDOUT the string argument.
+
+function displayMessage(message){
+    process.stdout.write(message + '\n');
+}
+
+module.exports = displayMessage;
