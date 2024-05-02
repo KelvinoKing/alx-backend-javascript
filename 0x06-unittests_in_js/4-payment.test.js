e0c123d2bb6ace6dbc3ab31c17ebb45f8b02d5f5
KelvinoKing <kelvinoking@outlook.com>
2024-05-02 15:37:39 +0300

    Stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,18 +1,24 @@
-// Create a new file named 3-payment.test.js and add a new suite named
-// sendPaymentRequestToApi:
-// By using sinon.spy, make sure the math used for sendPaymentRequestToApi(100, 20)
-// is the same as Utils.calculateNumber('SUM', 100, 20) (validate the usage of the Utils function)
+// Stub the function Utils.calculateNumber to always return the same number 10
+// Verify that the stub is being called with type = SUM, a = 100, and b = 20
+// Add a spy to verify that console.log is logging the correct message The total is: 10
 
 const assert = require('assert');
 const sinon = require('sinon');
 const Utils = require('./utils');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function() {
-    it('should call Utils.calculateNumber', function() {
-        const spy = sinon.spy(Utils, 'calculateNumber');
+    it('should call Utils.calculateNumber and console.log', function() {
+        const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        const consoleSpy = sinon.spy(console, 'log');
+
         sendPaymentRequestToApi(100, 20);
-        assert(spy.calledOnceWithExactly('SUM', 100, 20));
-        spy.restore();
+
+        expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+        calculateNumberStub.restore();
+        consoleSpy.restore();
     });
 });
