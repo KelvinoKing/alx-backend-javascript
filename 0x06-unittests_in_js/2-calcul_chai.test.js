a3c923d1bf9c85c04d7b2178c25ac4305e9d2ee3
KelvinoKing <kelvinoking@outlook.com>
2024-05-02 13:11:16 +0300

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -9,119 +9,121 @@ const { expect } = require('chai');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function() {
-    it('should return the sum of two numbers', function() {
-        expect(calculateNumber('SUM', 1.4, 3.5)).to.equal(5);
+    describe('type == "SUM"', function() {
+        it('should return the sum of two numbers', function() {
+            expect(calculateNumber('SUM', 1.4, 3.5)).to.equal(5);
+        });
+    
+        it('should return the sum of two numbers', function() {
+            expect(calculateNumber('SUM', 1.5, 3.5)).to.equal(6);
+        });
+    
+        it('should return the sum of two numbers', function() {
+            expect(calculateNumber('SUM', 1.4, 3.4)).to.equal(4);
+        });
+    
+        it('should return the sum of two numbers', function() {
+            expect(calculateNumber('SUM', 1.4, 3.6)).to.equal(5);
+        });
+    
+        it('should return the sum of two numbers', function() {
+            expect(calculateNumber('SUM', -1.4, 3.2)).to.equal(2);
+        });
+    
+        it('should return the sum of two numbers', function() {
+            expect(calculateNumber('SUM', -1.4, -3.2)).to.equal(-4);
+        });
+    
+        it('should return the sum of two numbers', function() {
+            expect(calculateNumber('SUM', 1.4, -3.5)).to.equal(-2);
+        });
     });
 
-    it('should return the sum of two numbers', function() {
-        expect(calculateNumber('SUM', 1.5, 3.5)).to.equal(6);
+    describe('type == "SUBTRACT"', function() {
+        it('should return the subtraction of two numbers', function() {
+            expect(calculateNumber('SUBTRACT', 1.4, 3.5)).to.equal(-3);
+        });
+    
+        it("should return the subtraction of two numbers", function() {
+            expect(calculateNumber('SUBTRACT', 1.5, 2.4)).to.equal(0);
+        });
+    
+        it("should return the subtraction of two numbers", function() {
+            expect(calculateNumber('SUBTRACT', 3.4, 2.4)).to.equal(1);
+        });
+    
+        it("should return the subtraction of two numbers", function() {
+            expect(calculateNumber('SUBTRACT', -1.4, -3.6)).to.equal(3);
+        });
+    
+        it("should return the subtraction of two numbers", function() {
+            expect(calculateNumber('SUBTRACT', -1.4, 3.2)).to.equal(-4);
+        });
+    
+        it("should return the subtraction of two numbers", function() {
+            expect(calculateNumber('SUBTRACT', 1.4, -3.5)).to.equal(4);
+        });
     });
 
-    it('should return the sum of two numbers', function() {
-        expect(calculateNumber('SUM', 1.4, 3.4)).to.equal(4);
-    });
-
-    it('should return the sum of two numbers', function() {
-        expect(calculateNumber('SUM', 1.4, 3.6)).to.equal(5);
-    });
-
-    it('should return the sum of two numbers', function() {
-        expect(calculateNumber('SUM', -1.4, 3.2)).to.equal(2);
-    });
-
-    it('should return the sum of two numbers', function() {
-        expect(calculateNumber('SUM', -1.4, -3.2)).to.equal(-4);
-    });
-
-    it('should return the sum of two numbers', function() {
-        expect(calculateNumber('SUM', 1.4, -3.5)).to.equal(-2);
-    });
-
-    it('should return the subtraction of two numbers', function() {
-        expect(calculateNumber('SUBTRACT', 1.4, 3.5)).to.equal(-3);
-    });
-
-    it("should return the subtraction of two numbers", function() {
-        expect(calculateNumber('SUBTRACT', 1.5, 2.4)).to.equal(0);
-    });
-
-    it("should return the subtraction of two numbers", function() {
-        expect(calculateNumber('SUBTRACT', 3.4, 2.4)).to.equal(1);
-    });
-
-    it("should return the subtraction of two numbers", function() {
-        expect(calculateNumber('SUBTRACT', -1.4, -3.6)).to.equal(3);
-    });
-
-    it("should return the subtraction of two numbers", function() {
-        expect(calculateNumber('SUBTRACT', -1.4, 3.2)).to.equal(-4);
-    });
-
-    it("should return the subtraction of two numbers", function() {
-        expect(calculateNumber('SUBTRACT', 1.4, -3.5)).to.equal(4);
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('SUM', 1.4, 3.4)).to.equal(4);
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 1.4, 3.5)).to.equal(0.25);
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 1.5, 2.4)).to.equal(1);
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 3.4, 2.4)).to.equal(1.5);
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', -1.4, -3.6)).to.equal(0.25);
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', -1.4, 3.2)).to.equal(-0.3333333333333333);
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 1.4, -3.5)).to.equal(-0.3333333333333333);
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', -1.4, 0)).to.equal('Error');
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 1.4, 0.1)).to.equal('Error');
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 0, -0.1)).to.equal('Error');
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 0, 0.1)).to.equal('Error');
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 0.1, 0)).to.equal('Error');
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', -0.1, 0)).to.equal('Error');
-    });
-
-    it('should return the division of two numbers', function() {
-        expect(calculateNumber('DIVIDE', 0, 0.5)).to.equal(0);
+    describe('type == "DIVIDE"', function() {
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 1.4, 3.5)).to.equal(0.25);
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 1.5, 2.4)).to.equal(1);
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 3.4, 2.4)).to.equal(1.5);
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', -1.4, -3.6)).to.equal(0.25);
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', -1.4, 3.2)).to.equal(-0.3333333333333333);
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 1.4, -3.5)).to.equal(-0.3333333333333333);
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', -1.4, 0)).to.equal('Error');
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 1.4, 0.1)).to.equal('Error');
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 0, -0.1)).to.equal('Error');
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 0, 0.1)).to.equal('Error');
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 0.1, 0)).to.equal('Error');
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', -0.1, 0)).to.equal('Error');
+        });
+    
+        it('should return the division of two numbers', function() {
+            expect(calculateNumber('DIVIDE', 0, 0.5)).to.equal(0);
+        });
     });
 });
